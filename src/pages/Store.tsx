de4a7d3aa093106c0ef9bba515e2f6e030f17648
sahilmandoliya <mandoliyasahil@gmail.com>
2022-07-10 22:57:33 +0530

    Cart icon

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,14 @@
+import {Row, Col} from "react-bootstrap"
+import storeItems from "../data/items.json";
+
 export function Store(){
-    return <h1>Store</h1>
+    return (
+      <>
+        <h1>Store</h1>
+        <Row>{storeItems.map(item=>(
+            <Col>{JSON.stringify(item)}</Col>
+        ))}
+        </Row>
+      </>
+    )
 }
